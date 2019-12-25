f7ac197af2596caa7ba08bdbd8d0c121c798cd42
bryantdp14 <59212817+bryantdp14@users.noreply.github.com>
2019-12-24 18:33:27 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Analizador lexico</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+    <center><div>Disegned by Bryant Dorado</div><br>
+        
+        <form action="analizar.php" method="post">
+           Numero: <input type="text" name="n" value="">
+            <input type="submit" value="Buscar">
+        </form>
+        </center>
+    </body>
+</html>
